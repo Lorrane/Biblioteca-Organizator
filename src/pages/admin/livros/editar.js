f5c772e9114494c5ba118280f3bf14ed3b38e707
Lorrane Machado <lorranemachado21@gmail.com>
2022-06-08 17:37:25 -0300

    Organização e uso do form book

--- src/pages/admin/livros/editar.js
+++ src/pages/admin/livros/editar.js
@@ -6,11 +6,11 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
 import Rodape from '../../../components/rodape';
-import { Button, Checkbox, FormControl, InputLabel, ListItemText, MenuItem, Select, TextField } from '@mui/material';
-import { Cancel, Save } from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../../../utils/requests';
+import FormBook from '../../../components/form-book';
+import { useParams } from 'react-router-dom';
 
 
 
@@ -19,42 +19,16 @@ const mdTheme = createTheme();
 
 export default function EditarLivro() {
 
-    const [selectGenrer, setSelectGenrer] = React.useState([]);
-    const navigate = useNavigate();
+    const [book, setBook] = React.useState([]);
+    const params = useParams();
 
-    const names = [
-        'Aventura',
-        'Ação',
-        'Documentário',
-        'Ficção',
-        'Fantasia',
-        'Humor',
-        'Terror',
-        'Drama',
-        'Auto-Ajuda',
-        'Educação',
-    ];
 
-    const ITEM_HEIGHT = 48;
-    const ITEM_PADDING_TOP = 8;
-    const MenuProps = {
-        PaperProps: {
-            style: {
-                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                width: 250,
-            },
-        },
-    };
-
-    const handleSelectChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-        setSelectGenrer(
-            // On autofill we get a stringified value.
-            typeof value === 'string' ? value.split(',') : value,
-        );
-    };
+    React.useEffect(() => {
+        axios.get(`${BASE_URL}/book/${params.idLivro}`)
+            .then(response => {
+                setBook(response.data);
+            })
+    }, [params.idLivro])
 
     return (
         <ThemeProvider theme={mdTheme}>
@@ -75,61 +49,7 @@ export default function EditarLivro() {
                 >
                     <Toolbar />
                     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                        <Paper>
-                            <FormControl fullWidth sx={{ p: 2 }}>
-                                <TextField id="title" label="Título" variant="standard" />
-                            </FormControl>
-                            <FormControl fullWidth sx={{ p: 2 }}>
-                                <TextField id="subtitle" label="Subtítulo" variant="standard" />
-                            </FormControl>
-                            <FormControl fullWidth sx={{ p: 2 }}>
-                                <TextField id="author" label="Autor(es)" variant="standard" />
-                            </FormControl>
-                            <FormControl fullWidth sx={{ p: 2 }}>
-                                <TextField id="publishing" label="Editora" variant="standard" />
-                            </FormControl>
-                            <FormControl fullWidth sx={{ p: 2 }}>
-                                <InputLabel id='genrer-label'>Categorias</InputLabel>
-                                <Select
-                                    labelId='genrer-label'
-                                    id="genrer"
-                                    multiple
-                                    value={selectGenrer}
-                                    onChange={handleSelectChange}
-                                    MenuProps={MenuProps}
-                                    variant="standard"
-                                    renderValue={selected => selected.join(', ')}
-                                >
-                                    {names.map((name) => (
-                                        <MenuItem key={name} value={name}>
-                                            <Checkbox checked={selectGenrer.indexOf(name) > -1} />
-                                            <ListItemText primary={name} />
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                            <Container align='right'>
-                                <Button
-                                    startIcon={<Cancel />}
-                                    color='error'
-                                    variant="contained"
-                                    sx={{ m: 2 }}
-                                    size='large'
-                                    onClick={() => navigate('/livros')}
-                                >
-                                    Cancelar
-                                </Button>
-                                <Button
-                                    startIcon={<Save />}
-                                    variant="contained"
-                                    sx={{ m: 2 }}
-                                    size='large'
-                                >
-                                    Salvar
-                                </Button>
-                            </Container>
-                        </Paper>
-
+                        <FormBook book={book} buttonName='Confirmar edição' />
                         <Rodape sx={{ pt: 4 }} />
                     </Container>
                 </Box>
